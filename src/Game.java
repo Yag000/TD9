d5e6db8085a208b32a2dd8b30708b9effddd3a05
gdudilli <gabin.dudillieu@etu.u-paris.fr>
2022-11-22 12:20:13 +0100

    Add methods Player

--- src/Game.java
+++ src/Game.java
@@ -8,4 +8,40 @@ public class Game {
         p1.setAttackGrid(p2.getSelfGrid());
         p2.setAttackGrid(p1.getSelfGrid());
     }
+
+    public void tourPlayer(Player p) {
+        String[] command;
+        do {
+            System.out.println("Ou voulez vous attaquer (a A1)");
+            try {
+                command = Parseur.parseInput();
+                int x = ((int) command[1].charAt(0)) - ((int) 'A');
+                int y = Integer.valueOf(command[1].charAt(1));
+
+                if (!p.getAttackGrid().isAlreadyShooted(x, y)) {
+                    p.attack(x, y);
+                    return;
+                }
+                System.out.println("Cette case a déjà été touchée.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        } while (true);
+    }
+
+    public void partie(){
+        boolean gagner = false;
+
+        do{
+            tourPlayer(p1);
+            gagner = p1.haveWin();
+
+            if(!gagner){
+                tourPlayer(p2);
+                gagner = p1.haveWin();
+            }
+        }
+
+        return;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -12,11 +12,28 @@ public class Player {
         return selfGrid;
     }
 
+    public Board getAttackGrid() {
+        return attackGrid;
+    }
+
     public void setAttackGrid(Board attackGrid) {
         this.attackGrid = attackGrid;
     }
 
-    public void affiche() {
+    public void attack(int x, int y) {
+
+    }
+
+    public boolean haveWin() {
+        if (attackGrid.allDestroy()) {
+            System.out.println(name + " a gagné !");
+            return true;
+        }
+        return false;
+    }
+
+    public void print() {
+        System.out.println(name + "\n");
         selfGrid.printSelf();
         attackGrid.printAttack();
     }
