930c36c5fc642eabe70ed73625cabe89cd3ea2c6
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-22 12:10:44 +0100

    Place command start

--- src/Parser.java
+++ src/Parser.java
@@ -15,19 +15,26 @@ public class Parser {
     public String[] parseInput() throws IllegalArgumentException {
         String nextString = sc.nextLine();
 
+        if (nextString.toUpperCase().matches("^[A-J]([1-9]|10)$"))
+            return new String[] { "SHOOT", nextString.toUpperCase() };
+
         String[] splitString = nextString.split(" ");
 
         switch (splitString[0].toUpperCase()) {
+
             case "SHOOT":
                 if (splitString.length != 2)
-                    throw new IllegalArgumentException();
+                    throw new IllegalArgumentException("Invalid number of arguments");
                 if (splitString[1].toUpperCase().matches("^[A-J]([1-9]|10)$"))
                     return new String[] { splitString[0].toUpperCase(), splitString[1].toUpperCase() };
                 else
-                    throw new IllegalArgumentException();
+                    throw new IllegalArgumentException("Invalid coordinates");
 
+            case "PLACE":
+                if (splitString.length != 3)
+                    throw new IllegalAccessError("Invalid number of arguments");
             default:
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Invalid command");
         }
 
     }
