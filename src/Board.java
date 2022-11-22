d0de1f1a05ab5b9a3429269326f43dc71f3b1295
GitFenixZ <soantonyly@gmail.com>
2022-11-22 11:51:11 +0100

    init

--- src/Board.java
+++ src/Board.java
@@ -22,12 +22,13 @@ public class Board {
     }
 
     public boolean placeBoat(int x, int y, Boat b, Orientation o) {
+        int verticalDirection = (o == Orientation.NORTH) ? 1 : -1;
+        int horizontalDirection = (o == Orientation.EAST) ? 1 : -1;
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
+        for (int i = x; i < board.length; i += verticalDirection) {
+            for (int j = y; j < board.length; j += horizontalDirection) {
 
             }
-
         }
 
     }
