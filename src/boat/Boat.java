1c5404c1b18b84a9e9129affe18030ea9fb8ed5f
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2022-11-22 11:35:58 +0100

    Add some default methods

--- src/boat/Boat.java
+++ src/boat/Boat.java
@@ -9,4 +9,38 @@ public abstract class Boat {
         this.size = size;
         this.hit = new boolean[size];
     }
+
+    /**
+     * Returns true if the boat is sunk, false otherwise.
+     * 
+     * @return true if the boat is sunk, false otherwise.
+     */
+    public boolean isSunk() {
+        for (boolean b : hit) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns true if the boat is hit at the given position, false otherwise.
+     * 
+     * @param position
+     *                 the position to check.
+     * @return true if the boat is hit at the given position, false otherwise.
+     */
+    public boolean isHit(int position) {
+        return hit[position];
+    }
+
+    /**
+     * The boat is hit at the given position.
+     * 
+     * @param position the position to hit.
+     */
+    public void hitAt(int position) {
+        hit[position] = true;
+    }
 }
